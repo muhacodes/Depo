09979c53f97e6b11deb0a42f2b044eca77e5de88
mohacodes <muha.oq3@gmail.com>
2022-01-25 09:04:29 +0300

    v1.0.1

--- project/views.py
+++ project/views.py
@@ -9,14 +9,16 @@ from django.db.models import Sum, F
 
 def home(request):
 
-    total_products = products.objects.all().aggregate(Sum('cost_price'))
+    total_products = products.objects.all().count()
     total_expense = Expense.objects.all().aggregate(Sum('amount'))
     total_sales = Expense.objects.all().aggregate(Sum('amount'))
-    
+    total_number_of_sales =  Sale.objects.all().count()
+
     context = {
-        'products' : total_products['cost_price__sum'],
+        'products' : total_products,
         'expense' : total_expense['amount__sum'],
-        'total_sales' : Sale.objects.filter().aggregate(sum=Sum(F('quantity')*F('selling_price')))["sum"]
+        'total_sales' : Sale.objects.filter().aggregate(sum=Sum(F('quantity')*F('selling_price')))["sum"],
+        'sales_total' : total_number_of_sales,
     }
 
     return render(request, 'home.html', context)
